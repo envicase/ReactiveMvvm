515e2b81410c1c9197e420140ff362d69b787db8
Gyuwon Yi <gyuwon@tidcorp.com>
2015-08-30 23:01:16 +0900

    ModelTest 클래스를 추가합니다.

---
+++ src/ReactiveMvvm/ReactiveMvvm.Tests/Models/ModelTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace ReactiveMvvm.Tests.Models
+{
+    public class ModelTest
+    {
+        [Theory, AutoData]
+        public void SetIdCorrectly(string id, string userName, string bio)
+        {
+            var user = new User(id, userName, bio);
+            user.Id.Should().BeSameAs(id);
+        }
+    }
+}
---
+++ src/ReactiveMvvm/ReactiveMvvm.Tests/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ReactiveMvvm.Models;
+
+namespace ReactiveMvvm.Tests.Models
+{
+    public class User : Model<string>
+    {
+        public User(string id, string userName, string bio)
+            : base(id)
+        {
+            if (userName == null)
+            {
+                throw new ArgumentNullException(nameof(userName));
+            }
+            if (bio == null)
+            {
+                throw new ArgumentNullException(nameof(bio));
+            }
+
+            UserName = userName;
+            Bio = bio;
+        }
+
+        public string UserName { get; }
+
+        public string Bio { get; }
+    }
+}
