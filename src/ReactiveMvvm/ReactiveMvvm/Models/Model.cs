c5a83249efac913baf9feacaed3f86e963bbe7ff
Gyuwon Yi <gyuwon@tidcorp.com>
2015-08-30 15:50:12 +0900

    Model<TId> 추상 클래스를 추가합니다.

---
+++ src/ReactiveMvvm/ReactiveMvvm/Models/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReactiveMvvm.Models
+{
+    public abstract class Model<TId>
+        where TId : IEquatable<TId>
+    {
+        protected Model(TId id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            Id = id;
+        }
+
+        public TId Id { get; }
+    }
+}
