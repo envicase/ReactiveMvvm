ce83d40e5f3189d142f42366ce86415480504143
Gyuwon Yi <gyuwon@tidcorp.com>
2015-09-01 11:51:06 +0900

    WeakSubscription 클래스를 추가합니다.

---
+++ src/ReactiveMvvm/ReactiveMvvm/WeakSubscription.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ReactiveMvvm
+{
+    public static class WeakSubscription
+    {
+        public static WeakSubscription<T> Create<T>(
+            IObservable<T> observable, IObserver<T> observer)
+        {
+            if (observable == null)
+            {
+                throw new ArgumentNullException(nameof(observable));
+            }
+            if (observer == null)
+            {
+                throw new ArgumentNullException(nameof(observer));
+            }
+
+            return new WeakSubscription<T>(observable, observer);
+        }
+    }
+
+    public class WeakSubscription<T> : IDisposable
+    {
+        private readonly WeakReference<IObserver<T>> _reference;
+        private readonly IDisposable _subscription;
+
+        public WeakSubscription(
+            IObservable<T> observable, IObserver<T> observer)
+        {
+            if (observable == null)
+            {
+                throw new ArgumentNullException(nameof(observable));
+            }
+            if (observer == null)
+            {
+                throw new ArgumentNullException(nameof(observer));
+            }
+
+            _reference = new WeakReference<IObserver<T>>(observer);
+            _subscription = observable.Subscribe(OnNext, OnError, OnCompleted);
+        }
+
+        private void OnNext(T value)
+        {
+            IObserver<T> observer;
+            if (_reference.TryGetTarget(out observer))
+            {
+                observer.OnNext(value);
+            }
+            else
+            {
+                _subscription.Dispose();
+            }
+        }
+
+        private void OnError(Exception error)
+        {
+            IObserver<T> observer;
+            if (_reference.TryGetTarget(out observer))
+            {
+                observer.OnError(error);
+            }
+            else
+            {
+                _subscription.Dispose();
+            }
+        }
+
+        private void OnCompleted()
+        {
+            IObserver<T> observer;
+            if (_reference.TryGetTarget(out observer))
+            {
+                observer.OnCompleted();
+            }
+            else
+            {
+                _subscription.Dispose();
+            }
+        }
+
+        public void Dispose() => _subscription.Dispose();
+    }
+}
