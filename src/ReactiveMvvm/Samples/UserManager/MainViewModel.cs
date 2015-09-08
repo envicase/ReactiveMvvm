09a2d789d77b803344ea1bf4e6de2b3bf8f2a037
Gyuwon Yi <gyuwon@tidcorp.com>
2015-09-08 23:09:17 +0900

    UserManager 프로젝트에 MainViewModel 클래스를 추가합니다.

---
+++ src/ReactiveMvvm/Samples/UserManager/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ReactiveMvvm;
+
+namespace UserManager
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            var users = new User[]
+            {
+                new User(1, "Tony Stark", "ironman@avengers.com"),
+                new User(2, "Bruce Banner", "hulk@avengers.com"),
+                new User(3, "Thor Odinson", "thor@avengers.com"),
+                new User(4, "Steve Rogers", "captain@avengers.com"),
+            };
+            Users = (from u in users
+                     select new UserItemViewModel(u)).ToList();
+        }
+
+        public IEnumerable<UserItemViewModel> Users { get; }
+    }
+}
