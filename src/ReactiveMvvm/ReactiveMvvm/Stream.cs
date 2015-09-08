b51a65106839925c920bcf4053ec64cc002473c6
Gyuwon Yi <gyuwon@tidcorp.com>
2015-09-08 21:29:01 +0900

    Stream<TModel, TId> 클래스에 대해 CA1001을 제외합니다.

--- src/ReactiveMvvm/ReactiveMvvm/Stream.cs
+++ src/ReactiveMvvm/ReactiveMvvm/Stream.cs
@@ -6,6 +6,10 @@ using System.Reactive.Subjects;
 
 namespace ReactiveMvvm
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Microsoft.Design",
+        "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable",
+        Justification = "Streams should not be disposed outside the class")]
     public sealed class Stream<TModel, TId> :
         ISubject<IObservable<TModel>, TModel>
         where TModel : Model<TId>
@@ -115,6 +119,9 @@ namespace ReactiveMvvm
         ~Stream()
         {
             Remove(Id);
+
+            _innerSubject.Dispose();
+            _spout.Dispose();
         }
 
         private InvalidOperationException InvalidCoalescingResultId =>
