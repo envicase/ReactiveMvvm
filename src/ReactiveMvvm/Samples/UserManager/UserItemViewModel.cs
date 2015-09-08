fe4434f9d316fd9ba1b1d169f79b5801bbc6a283
Gyuwon Yi <gyuwon@tidcorp.com>
2015-09-08 23:03:01 +0900

    UserManager 프로젝트에 UserItemViewModel 클래스를 추가합니다.

---
+++ src/ReactiveMvvm/Samples/UserManager/UserItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Linq;
+using ReactiveMvvm.ViewModels;
+
+namespace UserManager
+{
+    public class UserItemViewModel : ReactiveViewModel<User, int>
+    {
+        private static T ShouldNotBeNull<T>(T value, string paramName)
+            where T : class
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+
+            return value;
+        }
+
+        public UserItemViewModel(User user)
+            : base(ShouldNotBeNull(user, nameof(user)).Id)
+        {
+            Stream.OnNext(Observable.Return(user));
+        }
+
+        public UserItemViewModel(int id)
+            : base(id)
+        {
+        }
+    }
+}
