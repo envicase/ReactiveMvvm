502e8ce702e6ffcdce1e533bdf4da8afaef29ced
Gyuwon Yi <gyuwon@tidcorp.com>
2015-09-09 16:50:55 +0900

    StreamExtensions 클래스에 대해 CS1591을 비활성화 시킵니다.

--- src/ReactiveMvvm/ReactiveMvvm/StreamExtensions.cs
+++ src/ReactiveMvvm/ReactiveMvvm/StreamExtensions.cs
@@ -3,6 +3,10 @@ using System.Reactive.Linq;
 
 namespace ReactiveMvvm
 {
+    // TODO: StreamExtensions 클래스의 디자인이 성숙 단계에 이르면
+    // XML 주석을 추가하고 아래 pragma 지시문을 제거합니다.
+#pragma warning disable CS1591
+
     public static class StreamExtensions
     {
         public static void OnNext<TModel, TId>(
