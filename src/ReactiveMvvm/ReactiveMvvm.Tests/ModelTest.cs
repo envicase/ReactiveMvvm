183ceb51c3189f700198e7d1fe3ca96f50d9a7de
Gyuwon Yi <gyuwon@tidcorp.com>
2015-09-11 19:28:46 +0900

    테스트 메서드 이름의 오류를 수정합니다.

--- src/ReactiveMvvm/ReactiveMvvm.Tests/ModelTest.cs
+++ src/ReactiveMvvm/ReactiveMvvm.Tests/ModelTest.cs
@@ -8,7 +8,7 @@ namespace ReactiveMvvm.Tests
     public class ModelTest
     {
         [Theory, AutoData]
-        public void SetIdCorrectly(string id, string userName, string bio)
+        public void SetsIdCorrectly(string id, string userName, string bio)
         {
             var user = new User(id, userName, bio);
             user.Id.Should().BeSameAs(id);
