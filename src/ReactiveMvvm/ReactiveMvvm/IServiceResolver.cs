e93b224407734d7156df63cec577449acec87944
Gyuwon Yi <gyuwon@tidcorp.com>
2015-09-11 19:28:04 +0900

    ILocator 인터페이스를 IServiceResolver로 이름을 변경합니다.

--- src/ReactiveMvvm/ReactiveMvvm/IServiceResolver.cs
+++ src/ReactiveMvvm/ReactiveMvvm/IServiceResolver.cs
@@ -5,7 +5,7 @@ namespace ReactiveMvvm
     /// <summary>
     /// 의존성 서비스를 제공하는 기능을 정의합니다.
     /// </summary>
-    public interface ILocator : IServiceProvider
+    public interface IServiceResolver : IServiceProvider
     {
         /// <summary>
         /// 지정한 형식의 서비스 개체를 가져옵니다.
