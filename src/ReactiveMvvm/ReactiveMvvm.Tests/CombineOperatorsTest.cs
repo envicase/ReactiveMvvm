028f8253670607b33d76b84961995fe8fef1fb80
Gyuwon Yi <gyuwon@tidcorp.com>
2015-09-12 17:45:01 +0900

    CombineOperators 클래스를 추가합니다.

---
+++ src/ReactiveMvvm/ReactiveMvvm.Tests/CombineOperatorsTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace ReactiveMvvm.Tests
+{
+    public class CombineOperatorsTest
+    {
+        [Theory]
+        [InlineData(true, true, true)]
+        [InlineData(true, false, true)]
+        [InlineData(false, true, true)]
+        [InlineData(false, false, false)]
+        public void OrWith2ArgsReturnsCorrectValue(
+            bool arg1, bool arg2, bool expected)
+        {
+            CombineOperators.Or(arg1, arg2).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(true, true, true, true)]
+        [InlineData(true, true, false, true)]
+        [InlineData(true, false, true, true)]
+        [InlineData(true, false, false, true)]
+        [InlineData(false, true, true, true)]
+        [InlineData(false, true, false, true)]
+        [InlineData(false, false, true, true)]
+        [InlineData(false, false, false, false)]
+        public void OrWith3ArgsReturnsCorrectValue(
+            bool arg1, bool arg2, bool arg3, bool expected)
+        {
+            CombineOperators.Or(arg1, arg2, arg3).Should().Be(expected);
+        }
+
+        [Theory, AutoData]
+        public void OrWith4ArgsReturnsCorrectValue(
+            bool arg1, bool arg2, bool arg3, bool arg4)
+        {
+            CombineOperators.Or(arg1, arg2, arg3, arg4).Should()
+                .Be(arg1 || arg2 || arg3 || arg4);
+        }
+
+        [Theory]
+        [InlineData(true, true, true)]
+        [InlineData(true, false, false)]
+        [InlineData(false, true, false)]
+        [InlineData(false, false, false)]
+        public void AndWith2ArgsReturnsCorrectValue(
+            bool arg1, bool arg2, bool expected)
+        {
+            CombineOperators.And(arg1, arg2).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(true, true, true, true)]
+        [InlineData(true, true, false, false)]
+        [InlineData(true, false, true, false)]
+        [InlineData(true, false, false, false)]
+        [InlineData(false, true, true, false)]
+        [InlineData(false, true, false, false)]
+        [InlineData(false, false, true, false)]
+        [InlineData(false, false, false, false)]
+        public void AndWith3ArgsReturnsCorrectValue(
+            bool arg1, bool arg2, bool arg3, bool expected)
+        {
+            CombineOperators.And(arg1, arg2, arg3).Should().Be(expected);
+        }
+
+        [Theory, AutoData]
+        public void AndWith4ArgsReturnsCorrectValue(
+            bool arg1, bool arg2, bool arg3, bool arg4)
+        {
+            CombineOperators.And(arg1, arg2, arg3, arg4).Should()
+                .Be(arg1 && arg2 && arg3 && arg4);
+        }
+    }
+}
---
+++ src/ReactiveMvvm/ReactiveMvvm/CombineOperators.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReactiveMvvm
+{
+    // TODO: CombineOperators 클래스 디자인이 성숙단계에 이르면
+    // XML 주석을 추가하고 아래 pragma 지시문을 삭제합니다.
+#pragma warning disable CS1591
+
+    public static class CombineOperators
+    {
+        public static bool Or(bool arg1, bool arg2) => arg1 || arg2;
+
+        public static bool Or(bool arg1, bool arg2, bool arg3)
+            => arg1 || arg2 || arg3;
+
+        public static bool Or(bool arg1, bool arg2, bool arg3, bool arg4)
+            => arg1 || arg2 || arg3 || arg4;
+
+        public static bool And(bool arg1, bool arg2) => arg1 && arg2;
+
+        public static bool And(bool arg1, bool arg2, bool arg3)
+            => arg1 && arg2 && arg3;
+
+        public static bool And(bool arg1, bool arg2, bool arg3, bool arg4)
+            => arg1 && arg2 && arg3 && arg4;
+    }
+}
