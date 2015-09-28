9a3a2b813d921289c49d332317eef483eccf58e9
Gyuwon Yi <gyuwon@live.com>
2015-09-28 23:35:42 +0900

    ReactiveViewModel<TModel, TId> 클래스 생성자를 추가합니다.

---
+++ src/ReactiveMvvm/ConnectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ReactiveMvvm
+{
+    public static class ConnectionExtensions
+    {
+        public static void Send<TModel, TId>(
+            this IConnection<TModel, TId> connection, TModel model)
+            where TModel : class, IModel<TId>
+            where TId : IEquatable<TId>
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            connection.Send(Observable.Return(model));
+        }
+    }
+}
--- src/ReactiveMvvm/ViewModels/ReactiveViewModel.cs
+++ src/ReactiveMvvm/ViewModels/ReactiveViewModel.cs
@@ -6,22 +6,24 @@ namespace ReactiveMvvm.ViewModels
     // TODO: ReactiveViewModel 클래스의 디자인이 성숙 단계에 이르면
     // XML 주석을 추가하고 아래 pragma 지시문을 제거합니다.
 #pragma warning disable 1591
+
     public static class ReactiveViewModel
     {
-        public static PropertyChangedEventArgs ModelChangedEventArgs
-            { get; } = new PropertyChangedEventArgs("Model");
+        public static PropertyChangedEventArgs ModelChangedEventArgs { get; } =
+            new PropertyChangedEventArgs("Model");
     }
+
 #pragma warning restore 1591
 
     // TODO: ReactiveViewModel<TModel, TId> 클래스에 XML 주석이 작성되면 pragam
     // 지시문을 삭제해주세요.
 #pragma warning disable 1591
+
     public class ReactiveViewModel<TModel, TId> : ObservableObject, IDisposable
         where TModel : class, IModel<TId>
         where TId : IEquatable<TId>
     {
         private static T NullArgumentGuard<T>(T value, string paramName)
-            where T : class
         {
             if (value == null)
             {
@@ -43,6 +45,19 @@ namespace ReactiveMvvm.ViewModels
 
             _connection = connection;
             _connection.Subscribe(model => Model = model);
+        }
+
+        public ReactiveViewModel(TId modelId)
+            : this(Stream<TModel, TId>.Connect(
+                NullArgumentGuard(modelId, nameof(modelId))))
+        {
+        }
+
+        public ReactiveViewModel(TModel model)
+            : this(Stream<TModel, TId>.Connect(
+                NullArgumentGuard(model, nameof(model)).Id))
+        {
+            _connection.Send(model);
         }
 
         public TId ModelId => _connection.ModelId;
@@ -74,5 +89,6 @@ namespace ReactiveMvvm.ViewModels
 
         protected virtual void Dispose(bool disposing) => _connection.Dispose();
     }
+
 #pragma warning restore 1591
 }
