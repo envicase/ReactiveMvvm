75a118ba1f55958d43af097241ee8dacf23f98a3
Gyuwon Yi <gyuwon@live.com>
2015-09-28 19:39:20 +0900

    IConnection<TModel, TId> 인터페이스에 Send() 메서드를 추가합니다.

--- src/ReactiveMvvm/IConnection.cs
+++ src/ReactiveMvvm/IConnection.cs
@@ -15,5 +15,11 @@ namespace ReactiveMvvm
         /// 스트림에 의해 처리되는 모델 식별자를 가져옵니다.
         /// </summary>
         TId ModelId { get; }
+
+        /// <summary>
+        /// 새 모델 개정의 소스를 스트림에 전달합니다.
+        /// </summary>
+        /// <param name="source">모델 개정 소스입니다.</param>
+        void Send(IObservable<TModel> source);
     }
 }
